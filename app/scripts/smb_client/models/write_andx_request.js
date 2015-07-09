68c2efc070d10f547b63e45d11ed2625df395346
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2015-07-09 09:08:41 +0900

    Fixed the issue at writing/reading a file which the size is over 2GB.

--- app/scripts/smb_client/models/write_andx_request.js
+++ app/scripts/smb_client/models/write_andx_request.js
@@ -40,7 +40,7 @@
         // FID
         this.types_.setFixed2BytesValue(this.fid_, array, 4);
         // Offset Low
-        var offsetInfo = this.requestUtils_.divide8BytesValue(this.offset_);
+        var offsetInfo = this.types_.divide8BytesValue(this.offset_);
         this.types_.setFixed4BytesValue(offsetInfo.low, array, 6);
         // Timeout
         this.types_.setFixed4BytesValue(0, array, 10);
